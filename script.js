453e33ddf5ada89dca075b9d5abd4cd115e488aa
talhaker <tal.haker@gmail.com>
2018-04-08 14:41:15 +0300

    Exercise 3

--- script.js
+++ script.js
@@ -9,10 +9,20 @@ let addBeer = function(name, category) {
     // alert('Adding beer ' + name + ' into categiry ' + category);
 }
 
+let renderBeers = function() {
+    // Empty current content of beers list
+    let beersList = $('.beers-list');
+    beersList.empty();
+    beers.forEach(function(beer) {
+        $("<li>" + beer.beerName + ", " + beer.beerCategory + "</li>").appendTo(beersList);
+    });
+}
+
 let postBeerHandler = function() {
     let name = $('.beer-input').val();
     let category = $('.category-input').val();
     addBeer(name, category);
+    renderBeers();
 }
 
 $('.post-beer').click(postBeerHandler);
